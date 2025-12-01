0872c55b4a9071c6813007f783dfc283240a81cf
drkoustavsinharay <drkoustavsinharay@gmail.com>
2025-12-01 16:29:03 +0000

    babel compiled into dist

---
+++ dist/component1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { jsx as _jsx } from "react/jsx-runtime";
+function FirstComponent() {
+  return /*#__PURE__*/_jsx("div", {
+    children: /*#__PURE__*/_jsx("p", {
+      children: "This is a dummy paragraph."
+    })
+  });
+}
+export default FirstComponent;
---
+++ dist/index.js
@@ -0,0 +1,1 @@
+export { default as FirstComponent } from "./components/component1";
